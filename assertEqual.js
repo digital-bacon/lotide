4ef8b3cd0e6f1a11e77416c905b1a99a0e6658ff
Mark Bieganek <mark.bieganek@telus.com>
2022-11-14 14:52:50 -0700

    Add JSDoc documentation

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,10 @@
+/**
+ * Function that compares 2 values for equality and prints out a
+ * message telling us if they match or not.
+ * @param {string|number} actual - The first item to compare
+ * @param {string|number} expected - The second item to compare
+ * @returns {string} Prints message with match outcome
+ */
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log(`\n\x1b[32mTEST PASSED🥳🥳🥳\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
