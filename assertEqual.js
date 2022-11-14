8670ad4021aa86d6b1801a5cf75b5c406fc629e6
Mark Bieganek <mark.bieganek@telus.com>
2022-11-14 14:47:20 -0700

    Return result of assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,9 @@
 const assertEqual = (actual, expected) => {
-
+  if (actual === expected) {
+    console.log(`\n\x1b[32mTEST PASSED🥳🥳🥳\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
+  } else {
+    console.log(`\n\x1b[33mTEST FAILED💥💥💥\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
+  }
 };
 
 // TEST CODE
