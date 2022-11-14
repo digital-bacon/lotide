65ae0f1497b0732a3f89445b7d748293e13e44e8
Mark Bieganek <mark.bieganek@telus.com>
2022-11-14 15:26:15 -0700

    Add test cases for empty array and no array

--- head.js
+++ head.js
@@ -24,3 +24,5 @@ const head = (array) => Array.isArray(array) && array.length > 0 ? array[0] : un
 // TEST CASES
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head(), undefined);
