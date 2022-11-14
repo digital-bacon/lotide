e88a8acaccc88e7a10bc9968bf9ce890eb93d30a
Mark Bieganek <mark.bieganek@telus.com>
2022-11-14 15:45:21 -0700

    Convert function to export type

--- assertEqual.js
+++ assertEqual.js
@@ -5,16 +5,10 @@
  * @param {string|number} expected - The second item to compare
  * @returns {string} Prints message with match outcome
  */
-const assertEqual = (actual, expected) => {
+ export const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log(`\n\x1b[32mTEST PASSED🥳🥳🥳\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
   } else {
     console.log(`\n\x1b[33mTEST FAILED💥💥💥\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(0, 1);
