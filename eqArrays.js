776625ccaa2b8f215f6f4507c1e4ea43030bd5f2
Mark Bieganek <mark.bieganek@telus.com>
2022-11-15 17:43:45 -0700

    Fix: JSDoc comment accuracy

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,8 @@
 /**
- * Function that compares 2 values for equality and prints out a
+ * Function that compares 2 arrays for equality and prints out a
  * message telling us if they match or not
- * @param {string|number|boolean|Array<string|number|boolean>} actual - The first item to compare
- * @param {string|number|boolean|Array<string|number|boolean>} expected - The second item to compare
+ * @param {Array<string|number|boolean>} actual - The first item to compare
+ * @param {Array<string|number|boolean>} expected - The second item to compare
  * @returns {string} Prints message with match outcome
  */
 const eqArrays = (actual, expected) => {
