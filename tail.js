2dbabf14a82f6b25347524c194e4ea8d63a52ca1
Mark Bieganek <mark.bieganek@telus.com>
2022-11-15 20:00:42 -0700

    Replace old versions of assertEqual() where found

--- tail.js
+++ tail.js
@@ -1,33 +1,53 @@
 /**
  * Function that compares 2 values for equality and prints out a
  * message telling us if they match or not
- * @param {string|number|boolean|Array<string|number|boolean>} actual - The first item to compare
- * @param {string|number|boolean|Array<string|number|boolean>} expected - The second item to compare
+ * @param {string|number|boolean} actual - The first item to compare
+ * @param {string|number|boolean} expected - The second item to compare
  * @returns {string} Prints message with match outcome
  */
 const assertEqual = (actual, expected) => {
-  let hasEquality = false;
-  // If the items being asserted are arrays...
-  if (Array.isArray(actual) && Array.isArray(expected)) {
-    // Ensure length of the arrays are as expected
-    if (actual.length === expected.length) {
-      // Evaluate each element for equality
-      let totalEqualElements = 0;
-      for (const element in expected) {
-        if (element !== actual[element]) totalEqualElements++;
-      }
-      if (totalEqualElements === expected.length) hasEquality = true;
-    }
-  // If the items being asserted are not arrays or objects
-  } else {
-    if (actual === expected) hasEquality = true;
-  }
-  if (hasEquality) {
-    console.log(`\n\x1b[32mTEST PASSED🥳🥳🥳\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
-  } else {
-    console.log(`\n\x1b[33mTEST FAILED💥💥💥\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
-  }
+  console.log(printAssertEqualResult((actual === expected), actual, expected));
 };
+
+/**
+ * Function that builds a message for the assertEqual functions in this
+ * library
+ * @param {boolean} hasEquality - Result of equality check
+ * @param {*} actual - Input passed to the equality check
+ * @param {*} expected - Expected input in the equality check
+ * @returns {string} A formatted message
+ */
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+
+  // Color settings
+  const colorReset = '\x1b[0m'; // Reset colors
+  const colorMessageSuccess = '\x1b[32m'; // Green
+  const colorMessageFailure = '\x1b[33m'; // Yellow
+  const colorLabelResult = '\x1b[36m'; // Blue
+  const colorLabelExpected = '\x1b[36m'; // Blue
+
+  // Messages
+  const messageBefore = '\n';
+  const messageSuccess = colorMessageSuccess + `TEST PASSED🥳🥳🥳\n` + colorReset;
+  const messageFailure = colorMessageFailure + `TEST FAILED💥💥💥\n` + colorReset;
+  const messageResult = colorLabelResult + `result:\n` + colorReset + `${actual}\n`;
+  const messageExpected = colorLabelExpected + `expected:\n` + colorReset + `${expected}\n`;
+  const messageAfter = `\n----------`;
+
+  // Build the message to show the user
+  let message = '';
+  message += messageBefore;
+  if (hasEquality) {
+    message += messageSuccess;
+  } else {
+    message += messageFailure;
+  }
+  message += messageResult;
+  message += messageExpected;
+  message += messageAfter;
+  return message;
+};
+
 
 /**
  * Function that returns all but the first element from an array, or
