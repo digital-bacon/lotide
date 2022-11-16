9c8bb9c7b27cd2daf5e41a58af081728171ac062
Mark Bieganek <mark.bieganek@telus.com>
2022-11-16 14:46:43 -0700

    Add eqObjects.js and intial test cases

---
+++ eqObjects.js
@@ -0,0 +1,70 @@
+/**
+ * Function that compares 2 values for equality and prints out a
+ * message telling us if they match or not
+ * @param {string|number|boolean} actual - The first item to compare
+ * @param {string|number|boolean} expected - The second item to compare
+ * @returns {string} Prints message with match outcome
+ */
+ const assertEqual = (actual, expected) => {
+  console.log(printAssertEqualResult((actual === expected), actual, expected));
+};
+
+/**
+ * Function that builds a message for the assertEqual functions in this
+ * library
+ * @param {boolean} hasEquality - Result of equality check
+ * @param {*} actual - Input passed to the equality check
+ * @param {*} expected - Expected input in the equality check
+ * @returns {string} A formatted message
+ */
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+
+  // Color settings
+  const color = {
+    reset: '\x1b[0m', // Reset colors
+    messageSuccess: '\x1b[32m', // Green
+    messageFailure: '\x1b[33m', // Yellow
+    labelResult: '\x1b[36m', // Blue
+    labelExpected: '\x1b[36m', // Blue
+  };
+  
+  // Message configuration
+  const message = {
+    before: '\n',
+    success: color.messageSuccess + `TEST PASSED🥳🥳🥳\n` + color.reset,
+    failure: color.messageFailure + `TEST FAILED💥💥💥\n` + color.reset,
+    result: color.labelResult + `result:\n` + color.reset + `${actual}\n`,
+    expected: color.labelExpected + `expected:\n` + color.reset + `${expected}\n`,
+    after: `\n----------`,
+    buildMessage: function() {
+      let message = '';
+      message += this.before;
+      message += hasEquality ? this.success : this.failure;
+      message += this.result;
+      message += this.expected;
+      message += this.after;
+      return message;
+    }
+  };
+
+  // Build the message to show the user
+  return message.buildMessage();
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+/**
+ * Function that evaluates two provided objects for equality
+ * @param {Object} object1 - The first object to evaluate
+ * @param {Object} object2 - The second object to evaluate
+ */
+const eqObjects = (object1, object2) => {
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
