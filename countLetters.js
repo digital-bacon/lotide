b19c317cd8fac2b250f7b2d426e7712811a3c69d
Mark Bieganek <mark.bieganek@telus.com>
2022-11-16 10:48:33 -0700

    Add functional logic, linted

---
+++ countLetters.js
@@ -0,0 +1,63 @@
+/**
+ * Function that compares 2 values for equality and prints out a
+ * message telling us if they match or not
+ * @param {string|number|boolean} actual - The first item to compare
+ * @param {string|number|boolean} expected - The second item to compare
+ * @returns {string} Prints message with match outcome
+ */
+const assertEqual = (actual, expected) => {
+  console.log(printAssertEqualResult((actual === expected), actual, expected));
+};
+
+/**
+ * Function that builds a message for the assertEqual functions in this
+ * library
+ * @param {boolean} hasEquality - Result of equality check
+ * @param {*} actual - Input passed to the equality check
+ * @param {*} expected - Expected input in the equality check
+ * @returns {string} A formatted message
+ */
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+
+  // Color settings
+  const colorReset = '\x1b[0m'; // Reset colors
+  const colorMessageSuccess = '\x1b[32m'; // Green
+  const colorMessageFailure = '\x1b[33m'; // Yellow
+  const colorLabelResult = '\x1b[36m'; // Blue
+  const colorLabelExpected = '\x1b[36m'; // Blue
+
+  // Messages
+  const messageBefore = '\n';
+  const messageSuccess = colorMessageSuccess + `TEST PASSED🥳🥳🥳\n` + colorReset;
+  const messageFailure = colorMessageFailure + `TEST FAILED💥💥💥\n` + colorReset;
+  const messageResult = colorLabelResult + `result:\n` + colorReset + `${actual}\n`;
+  const messageExpected = colorLabelExpected + `expected:\n` + colorReset + `${expected}\n`;
+  const messageAfter = `\n----------`;
+
+  // Build the message to show the user
+  let message = '';
+  message += messageBefore;
+  if (hasEquality) {
+    message += messageSuccess;
+  } else {
+    message += messageFailure;
+  }
+  message += messageResult;
+  message += messageExpected;
+  message += messageAfter;
+  return message;
+};
+
+const countLetters = (string) => {
+  const result = {}; // An object to hold the results
+  // Loop through provided string
+  for (const character of string) {
+    // Increment the count of this character in the result object by 1
+    // If the character doesn't exist in the result object, create it
+    result[character] ? result[character] += 1 : result[character] = 1;
+  }
+  return result;
+};
+
+assertEqual(countLetters("Apple")["A"], 1);
+assertEqual(countLetters("Apple")["p"], 2);
