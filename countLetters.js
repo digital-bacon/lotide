27d87db7cd51e63699f39ec1697767e3f81f77a2
Mark Bieganek <mark.bieganek@telus.com>
2022-11-16 14:29:57 -0700

    Add JSDoc comments, handle spaces in countLetters()

--- countLetters.js
+++ countLetters.js
@@ -51,10 +51,20 @@ const printAssertEqualResult = (hasEquality, actual, expected) => {
   return message.buildMessage();
 };
 
+/**
+ * Function that counts the total instances of a letter in a string
+ * @param {string} string - The string containing the characters you
+ * wish to count
+ * @returns {Object} An object with characters as keys, and the total
+ * instances of that character in the provided string
+ */
 const countLetters = (string) => {
   const result = {}; // An object to hold the results
   // Loop through provided string
+  loopString:
   for (const character of string) {
+    // Don't count spaces
+    if (character === ' ') continue loopString;
     // Increment the count of this character in the result object by 1
     // If the character doesn't exist in the result object, create it
     result[character] ? result[character] += 1 : result[character] = 1;
