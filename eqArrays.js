c2aa027ac9c29c3f7a5e01cf6bc2575a5acf218e
Mark Bieganek <mark.bieganek@telus.com>
2022-11-15 17:42:54 -0700

    Add comments to for... loop for clarity

--- eqArrays.js
+++ eqArrays.js
@@ -14,11 +14,14 @@ const eqArrays = (actual, expected) => {
       // Evaluate each element for equality
       let totalEqualElements = 0;
       for (let element = 0; element < expected.length; element++) {
+        // Evaluate element type equality
         if (actual[element] === expected[element]) totalEqualElements++;
       }
+      // If the total equal elements matches expected, we have equality
       if (totalEqualElements === expected.length) hasEquality = true;
     }
   }
+  // Print the outcome!
   if (hasEquality) {
     console.log(`\n\x1b[32mTEST PASSED🥳🥳🥳\n\x1b[36mresult:\n\x1b[0m`, actual, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
   } else {
@@ -26,16 +29,9 @@ const eqArrays = (actual, expected) => {
   }
 };
 
-/**
- * Function that flattens an array of any depth to single-dimensional
- * @param {Array} array - The array to flatten
- * @returns {Array} The flattened, single-dimensional, array
- */
- const flattenArray = (array) => array.flat(Infinity);
-
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
