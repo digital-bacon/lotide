0b621cd3791bb11556b34ef246f5e5e904ae451c
Mark Bieganek <mark.bieganek@telus.com>
2022-11-17 13:50:18 -0700

    Add findKey.js first commit, with test cases

---
+++ findKey.js
@@ -0,0 +1,69 @@
+/**
+ * Function that compares 2 values for equality and prints out a
+ * message telling us if they match or not
+ * @param {string|number|boolean} actual - The first item to compare
+ * @param {string|number|boolean} expected - The second item to compare
+ * @returns {string} Prints message with match outcome
+ */
+ const assertEqual = (actual, expected) => {
+  console.log(printAssertEqualResult((actual === expected), actual, expected));
+};
+
+/**
+ * Function that builds a message for the assertEqual functions in this
+ * library
+ * @param {boolean} hasEquality - Result of equality check
+ * @param {*} actual - Input passed to the equality check
+ * @param {*} expected - Expected input in the equality check
+ * @returns {string} A formatted message
+ */
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+  
+  // To output objects with their kv pairs
+  const inspect = require('util').inspect;
+  
+  // Message configuration
+  const config = [
+    {type: 'before', color: '', text: '\n'},
+    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
+    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
+    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
+    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
+    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
+    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
+    {type: 'after', color: '', text: `\n----------`},
+  ];
+  
+  const resetColor = '\x1b[0m';
+  let message = '';
+  for (const object in config) {
+    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
+    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
+    message += config[object].color + config[object].text + resetColor;
+  }
+  return message;
+};
+
+/**
+ * Function that scans an object and returns the first key for which
+ * the callback/predicate returns a truthy value. If no key is found,
+ * then it returns `undefined`.
+ * @param {Object} object - The object from which to find a key
+ * @param {Function} callback - The callback function that is used to
+ * evaluate the keys
+ */
+const findKey = (object, action) => {
+  // Some logic here
+}
+
+// Test case:
+const testCaseObject = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+const testLogic = x => x.stars === 2;
+assertEqual(findKey(testCaseObject, testLogic), "noma");
