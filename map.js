77cc35d59e5004fd0592a01479d321f2cf082273
Mark Bieganek <mark.bieganek@telus.com>
2022-11-16 22:08:56 -0700

    Change to push the first letter rather than print

--- map.js
+++ map.js
@@ -1,14 +1,79 @@
-const words = ["ground", "control", "to", "major", "tom"];
+/**
+ * Function that compares 2 arrays for equality and prints out a
+ * message telling us if they match or not
+ * @param {Array<string|number|boolean>} array1 - The first array to compare
+ * @param {Array<string|number|boolean>} array2 - The second array to compare
+ * @returns {boolean} The result of the evaluation
+ */
+ const assertArraysEqual = (array1, array2) => {
+  return console.log(printAssertEqualResult(eqArrays(array1, array2), array1, array2));
+};
 
+/**
+ * Function that compares 2 arrays for equality and returns the result
+ * @param {Array<string|number|boolean>} array1 - The first array to compare
+ * @param {Array<string|number|boolean>} array2 - The second array to compare
+ * @returns {boolean} The result of the evaluation
+ */
+const eqArrays = (array1, array2) => {
+  // If the items being asserted are arrays...
+  if (!Array.isArray(array1) || !Array.isArray(array2)) return false;
+  // Ensure length of the arrays are as array2
+  if (array1.length !== array2.length) return false;
+  // Evaluate each element for equality
+  for (let element = 0; element < array2.length; element++) {
+    // Evaluate element type equality
+    if (array1[element] !== array2[element]) return false;
+  }
+  // No tests remain, so we have equality
+  return true;
+};
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+  
+  // To output objects with their kv pairs
+  const inspect = require('util').inspect;
+  
+  // Message configuration
+  const config = [
+    {type: 'before', color: '', text: '\n'},
+    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
+    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
+    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
+    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
+    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
+    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
+    {type: 'after', color: '', text: `\n----------`},
+  ]
+  
+  const resetColor = '\x1b[0m';
+  let message = '';
+  for (const object in config) {
+    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
+    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
+    message += config[object].color + config[object].text + resetColor;
+  }
+  return message;
+};
+
+
+/**
+ * Function that will take in two arguments, an array to map, and a
+ * callback function. The function will return a new array based on
+ * the results of the callback function.
+ * @param {Array} array - An array to map
+ * @param {Function} callback - A callback function to call against
+ * each item in the array
+ * @returns {Array} An array with the results of the callback function
+ */
 const map = (array, callback) => {
   const results = [];
   for (let item of array) {
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
+    results.push(callback(item));
   }
   return results;
-}
+};
 
+
+const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
