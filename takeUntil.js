20f9ec5f49d896f3bc9dbec5cb3d98efa5d01218
Mark Bieganek <mark.bieganek@telus.com>
2022-11-17 10:28:43 -0700

    Lint pass, add stopAtIndex variable to improve readability

--- takeUntil.js
+++ takeUntil.js
@@ -5,7 +5,7 @@
  * @param {Array<string|number|boolean>} array2 - The second array to compare
  * @returns {boolean} The result of the evaluation
  */
- const assertArraysEqual = (array1, array2) => {
+const assertArraysEqual = (array1, array2) => {
   return console.log(printAssertEqualResult(eqArrays(array1, array2), array1, array2));
 };
 
@@ -64,7 +64,6 @@ const printAssertEqualResult = (hasEquality, actual, expected) => {
   return message;
 };
 
-
 /**
  * Function that returns a slice of the provided array with elements
  * taken from the beginning, up until the callback/predicate returns
@@ -77,19 +76,21 @@ const printAssertEqualResult = (hasEquality, actual, expected) => {
 const takeUntil = function(array, callback) {
   // Create a way to store the results
   let output = [];
+  // Get the stopping point for the slice from the callback
+  const stopAtIndex = array.findIndex(callback);
   // Extract the items using the new rules
-  output = array.slice(0, array.findIndex(callback));
+  output = array.slice(0, stopAtIndex);
   // Return the new array
   return output;
-}
+};
 
 // TEST CASES
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 // Evaluate result against expected result
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]) // => True
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]); // => True
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 // Evaluate result against expected result
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]) // => True
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]); // => True
