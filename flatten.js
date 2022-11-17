265e311f25b92bc6acd9349243901ad46ab537c8
Mark Bieganek <mark.bieganek@telus.com>
2022-11-17 15:50:26 -0700

    Return flattened array

---
+++ flatten.js
@@ -0,0 +1,89 @@
+/**
+ * Function that compares 2 arrays for equality and prints out a
+ * message telling us if they match or not
+ * @param {Array<string|number|boolean>} array1 - The first array to compare
+ * @param {Array<string|number|boolean>} array2 - The second array to compare
+ * @returns {boolean} The result of the evaluation
+ */
+ const assertArraysEqual = (array1, array2) => {
+  return console.log(printAssertEqualResult(eqArrays(array1, array2), array1, array2));
+};
+
+/**
+ * Function that compares 2 arrays for equality and returns the result
+ * @param {Array<string|number|boolean>} array1 - The first array to compare
+ * @param {Array<string|number|boolean>} array2 - The second array to compare
+ * @returns {boolean} The result of the evaluation
+ */
+const eqArrays = (array1, array2) => {
+  // If the items being asserted are arrays...
+  if (!Array.isArray(array1) || !Array.isArray(array2)) return false;
+  // Ensure length of the arrays are as array2
+  if (array1.length !== array2.length) return false;
+  // Evaluate each element for equality
+  for (let element = 0; element < array2.length; element++) {
+    // Evaluate element type equality
+    if (array1[element] !== array2[element]) return false;
+  }
+  // No tests remain, so we have equality
+  return true;
+};
+
+/**
+ * Function that builds a message for the assertEqual functions in this
+ * library
+ * @param {boolean} hasEquality - Result of equality check
+ * @param {*} actual - Input passed to the equality check
+ * @param {*} expected - Expected input in the equality check
+ * @returns {string} A formatted message
+ */
+const printAssertEqualResult = (hasEquality, actual, expected) => {
+  
+  // To output objects with their kv pairs
+  const inspect = require('util').inspect;
+  
+  // Message configuration
+  const config = [
+    {type: 'before', color: '', text: '\n'},
+    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
+    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
+    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
+    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
+    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
+    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
+    {type: 'after', color: '', text: `\n----------`},
+  ];
+  
+  const resetColor = '\x1b[0m';
+  let message = '';
+  for (const object in config) {
+    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
+    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
+    message += config[object].color + config[object].text + resetColor;
+  }
+  return message;
+};
+
+const flatten = (array) => {
+  // Edge case: We need a way to check if the item is an array before flattening it
+  // Add a buffer for the result
+  const buffer = [];
+  // Loop through an existing array of items
+  for (const item of array) {
+    console.log(item)
+    // If a nested array is found
+    if (Array.isArray(item)) {
+      // Loop through an existing array of items
+      for (const nestedArrayItem of item) {
+        //Insert this element at the end of the buffer
+        buffer.push(nestedArrayItem);
+      }
+    } else {
+      // Insert this element at the end of the buffer
+      buffer.push(item);
+    }
+  }
+  return buffer;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]) // => [1, 2, 3, 4, 5, 6]
