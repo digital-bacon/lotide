262629941fb86acb62bdd0a0fdfc0a5e5561e6cf
Mark Bieganek <mark.bieganek@telus.com>
2022-11-17 16:15:32 -0700

    Refactor to support unlimited nested flattening

--- flatten.js
+++ flatten.js
@@ -64,25 +64,34 @@ const printAssertEqualResult = (hasEquality, actual, expected) => {
   return message;
 };
 
-const flatten = (array) => {
-  // Edge case: We need a way to check if the item is an array before flattening it
-  // Add a buffer for the result
-  const buffer = [];
+
+/**
+ * Function that flattens an array with any number of nested arrays
+ * @param {Array} array - The array of elements to flatten
+ * @param {Array} [buffer] - used as a pass-through value for recursion
+ * @returns {Array} The flattened array
+ */
+const flatten = (array, buffer) => {
+  if (typeof buffer === 'undefined') buffer = [];
   // Loop through an existing array of items
   for (const item of array) {
     // If a nested array is found
-    if (Array.isArray(item)) {
-      // Loop through an existing array of items
-      for (const nestedArrayItem of item) {
-        //Insert this element at the end of the buffer
-        buffer.push(nestedArrayItem);
-      }
-    } else {
-      // Insert this element at the end of the buffer
+    if (Array.isArray(item) === false) {
       buffer.push(item);
+    } else {
+      // Recursively flatten all nested elements at this index
+      flatten(item, buffer);
     }
   }
   return buffer;
 };
 
+// Test Case: 1 nested level of sub-arrays
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+// Test Case: 2 nested levels of sub-arrays
+assertArraysEqual(flatten([1, 2, [3, [1, 2, 3]], 4, 5, [6]]), [1, 2, 3, 1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+// Test Case: 3 nested levels of sub-arrays
+assertArraysEqual(flatten([1, 2, [3, [1, 2, 3, [1, 2, 3]]], 4, 5, [6]]), [1, 2, 3, 1, 2, 3, 1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+
+// Test Case: objects as array elements
+console.log(flatten([1, 2, {apple: 3, orange: 4}, 5, [6]]),  [1, 2, {apple: 3, orange: 4}, 5, 6]); // => [1, 2, 3, 4, 5, 6]
