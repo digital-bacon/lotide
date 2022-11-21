e0a80a683c6ba129846cce9af224ed2c25a93f47
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 14:17:53 -0700

    Refactor for modular assertion function references

--- eqObjects.js
+++ eqObjects.js
@@ -1,68 +1,5 @@
-/**
- * Function that compares 2 values for equality and prints out a
- * message telling us if they match or not
- * @param {string|number|boolean} actual - The first item to compare
- * @param {string|number|boolean} expected - The second item to compare
- * @returns {string} Prints message with match outcome
- */
-const assertEqual = (actual, expected) => {
-  console.log(printAssertEqualResult((actual === expected), actual, expected));
-};
-
-/**
- * Function that compares 2 arrays for equality and returns the result
- * @param {Array<string|number|boolean>} array1 - The first array to compare
- * @param {Array<string|number|boolean>} array2 - The second array to compare
- * @returns {boolean} The result of the evaluation
- */
-const eqArrays = (array1, array2) => {
-  // If the items being asserted are arrays...
-  if (!Array.isArray(array1) || !Array.isArray(array2)) return false;
-  // Ensure length of the arrays are as array2
-  if (array1.length !== array2.length) return false;
-  // Evaluate each element for equality
-  for (let element = 0; element < array2.length; element++) {
-    // Evaluate element type equality
-    if (array1[element] !== array2[element]) return false;
-  }
-  // No tests remain, so we have equality
-  return true;
-};
-
-/**
- * Function that builds a message for the assertEqual functions in this
- * library
- * @param {boolean} hasEquality - Result of equality check
- * @param {*} actual - Input passed to the equality check
- * @param {*} expected - Expected input in the equality check
- * @returns {string} A formatted message
- */
-const printAssertEqualResult = (hasEquality, actual, expected) => {
-  
-  // To output objects with their kv pairs
-  const inspect = require('util').inspect;
-  
-  // Message configuration
-  const config = [
-    {type: 'before', color: '', text: '\n'},
-    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
-    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
-    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
-    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
-    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
-    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
-    {type: 'after', color: '', text: `\n----------`},
-  ];
-  
-  const resetColor = '\x1b[0m';
-  let message = '';
-  for (const object in config) {
-    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
-    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
-    message += config[object].color + config[object].text + resetColor;
-  }
-  return message;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 /**
  * Function that evaluates two provided objects for equality
@@ -109,3 +46,5 @@ assertEqual(eqObjects(obj1, obj2), true); // => true
 const obj3 = { c: "1", d: ["2", 3] };
 const obj4 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(obj3, obj4), false); // => false
+
+module.exports = eqObjects;
