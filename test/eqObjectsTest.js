f63b0dcaa18ef147f428126bdc1beb098e73113b
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 16:32:37 -0700

    Fix: change array1 array2 to object1 object2

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -4,52 +4,52 @@ const eqObjects = require('../eqObjects');
 describe("#eqObjects", () => {
 
   it('should return true when objects are the same', () => {
-    const array1 = { a: "1", b: "2" };
-    const array2 = { a: "1", b: "2" };
+    const object1 = { a: "1", b: "2" };
+    const object2 = { a: "1", b: "2" };
     const expectedResult = true;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
   
   it('should return true when objects are the same, but elements order differs', () => {
-    const array1 = { a: "1", b: "2" };
-    const array2 = { b: "2", a: "1" };
+    const object1 = { a: "1", b: "2" };
+    const object2 = { b: "2", a: "1" };
     const expectedResult = true;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
   it('should return false when objects have a different number of values', () => {
-    const array1 = { a: "1", b: "2" };
-    const array2 = { a: "1", b: "2", c: "3" };
+    const object1 = { a: "1", b: "2" };
+    const object2 = { a: "1", b: "2", c: "3" };
     const expectedResult = false;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
   it('should return true when objects are the same and contain arrays', () => {
-    const array1 = { c: "1", d: ["2", 3] };
-    const array2 = { d: ["2", 3], c: "1" };
+    const object1 = { c: "1", d: ["2", 3] };
+    const object2 = { d: ["2", 3], c: "1" };
     const expectedResult = true;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
   it('should return false when objects contain arrays of with different values', () => {
-    const array1 = { c: "1", d: ["2", 3] };
-    const array2 = { c: "1", d: ["2", 4] };
+    const object1 = { c: "1", d: ["2", 3] };
+    const object2 = { c: "1", d: ["2", 4] };
     const expectedResult = false;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
   it('should return false when objects contain arrays of different lengths', () => {
-    const array1 = { c: "1", d: ["2", 3] };
-    const array2 = { c: "1", d: ["2", 3, 4] };
+    const object1 = { c: "1", d: ["2", 3] };
+    const object2 = { c: "1", d: ["2", 3, 4] };
     const expectedResult = false;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
   it('should return true when objectsare both empty', () => {
-    const array1 = {};
-    const array2 = {};
+    const object1 = {};
+    const object2 = {};
     const expectedResult = true;
-    assert.deepEqual(eqObjects(array1, array2), expectedResult);
+    assert.deepEqual(eqObjects(object1, object2), expectedResult);
   });
 
 });
