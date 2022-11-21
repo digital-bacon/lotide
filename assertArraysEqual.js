08030378eee1fd3abe73e876ef6d51b989c495b5
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 09:03:31 -0700

    Create modular unit tests for assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,6 @@
+const eqArrays = require('./eqArrays');
+const printAssertEqualResult = require('./printAssertEqualResult');
+
 /**
  * Function that compares 2 arrays for equality and prints out a
  * message telling us if they match or not
@@ -9,64 +12,4 @@ const assertArraysEqual = (array1, array2) => {
   return console.log(printAssertEqualResult(eqArrays(array1, array2), array1, array2));
 };
 
-/**
- * Function that compares 2 arrays for equality and returns the result
- * @param {Array<string|number|boolean>} array1 - The first array to compare
- * @param {Array<string|number|boolean>} array2 - The second array to compare
- * @returns {boolean} The result of the evaluation
- */
-const eqArrays = (array1, array2) => {
-  // If the items being asserted are arrays...
-  if (!Array.isArray(array1) || !Array.isArray(array2)) return false;
-  // Ensure length of the arrays are as array2
-  if (array1.length !== array2.length) return false;
-  // Evaluate each element for equality
-  for (let element = 0; element < array2.length; element++) {
-    // Evaluate element type equality
-    if (array1[element] !== array2[element]) return false;
-  }
-  // No tests remain, so we have equality
-  return true;
-};
-
-/**
- * Function that builds a message for the assertEqual functions in this
- * library
- * @param {boolean} hasEquality - Result of equality check
- * @param {*} actual - Input passed to the equality check
- * @param {*} expected - Expected input in the equality check
- * @returns {string} A formatted message
- */
-const printAssertEqualResult = (hasEquality, actual, expected) => {
-  
-  // To output objects with their kv pairs
-  const inspect = require('util').inspect;
-  
-  // Message configuration
-  const config = [
-    {type: 'before', color: '', text: '\n'},
-    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
-    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
-    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
-    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
-    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
-    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
-    {type: 'after', color: '', text: `\n----------`},
-  ];
-  
-  const resetColor = '\x1b[0m';
-  let message = '';
-  for (const object in config) {
-    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
-    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
-    message += config[object].color + config[object].text + resetColor;
-  }
-  return message;
-};
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
+module.exports = assertArraysEqual;
