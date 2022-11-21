9f2184df698fa5e8f6cf4ee8c1927df4f70f3bb2
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 15:45:55 -0700

    Convert unit tests to Mocha + Chai assertions

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,34 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+describe("#eqArrays", () => {
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+  it('should evaluate arrays as equal when they are the same', () => {
+    const array1 = [1, 2, 3];
+    const array2 = [1, 2, 3];
+    const expectedResult = true;
+    assert.deepEqual(eqArrays(array1, array2), expectedResult);
+  });
+
+  it('should evaluate arrays as not equal when they are different', () => {
+    const array1 = [1, 2, 3];
+    const array2 = [3, 2, 1];
+    const expectedResult = false;
+    assert.deepEqual(eqArrays(array1, array2), expectedResult);
+  });
+
+  it('should evaluate array values for type equality', () => {
+    const array1 = ["1", "2", "3"];
+    const array2 = ["1", "2", 3];
+    const expectedResult = false;
+    assert.deepEqual(eqArrays(array1, array2), expectedResult);
+  });
+
+  it('should evaluate arrays with mixed value types as equal when they are the same', () => {
+    const array1 = ["1", true, null];
+    const array2 = ["1", true, null];
+    const expectedResult = true;
+    assert.deepEqual(eqArrays(array1, array2), expectedResult);
+  });
+
+});
