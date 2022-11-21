c6b3e7a7799e2f35f9e0db71734996c5d9d54063
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 16:15:21 -0700

    Refactor unit tests to test/ folder

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -12,21 +12,4 @@ const assertObjectsEqual = (object1, object2) => {
   return console.log(printAssertEqualResult(eqObjects(object1, object2), object1, object2));
 };
 
-// // Test cases: Primitives as property values
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertObjectsEqual(ab, ba); // => true
-
-// const abc = { a: "1", b: "2", c: "3" };
-// assertObjectsEqual(ab, abc); // => false
-
-// // Test cases: Arrays in property values
-// const obj1 = { c: "1", d: ["2", 3] };
-// const obj2 = { d: ["2", 3], c: "1" };
-// assertObjectsEqual(obj1, obj2); // => true
-
-// const obj3 = { c: "1", d: ["2", 3] };
-// const obj4 = { c: "1", d: ["2", 3, 4] };
-// assertObjectsEqual(obj3, obj4); // => false
-
 module.exports = assertObjectsEqual;
---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,19 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+// TEST CODE
+// Test cases: Primitives as property values
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
+
+// Test cases: Arrays in property values
+const obj1 = { c: "1", d: ["2", 3] };
+const obj2 = { d: ["2", 3], c: "1" };
+assertObjectsEqual(obj1, obj2); // => true
+
+const obj3 = { c: "1", d: ["2", 3] };
+const obj4 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(obj3, obj4); // => false
