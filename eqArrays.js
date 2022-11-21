cc8bae040f1f05aecde7920db275b9b97c9e5df9
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 08:49:58 -0700

    Create modular unit tests for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,50 +1,4 @@
 /**
- * Function that compares 2 values for equality and prints out a
- * message telling us if they match or not
- * @param {string|number|boolean} actual - The first item to compare
- * @param {string|number|boolean} expected - The second item to compare
- * @returns {string} Prints message with match outcome
- */
-const assertEqual = (actual, expected) => {
-  console.log(printAssertEqualResult((actual === expected), actual, expected));
-};
-
-/**
- * Function that builds a message for the assertEqual functions in this
- * library
- * @param {boolean} hasEquality - Result of equality check
- * @param {*} actual - Input passed to the equality check
- * @param {*} expected - Expected input in the equality check
- * @returns {string} A formatted message
- */
-const printAssertEqualResult = (hasEquality, actual, expected) => {
-  
-  // To output objects with their kv pairs
-  const inspect = require('util').inspect;
-  
-  // Message configuration
-  const config = [
-    {type: 'before', color: '', text: '\n'},
-    {type: 'success', color: '\x1b[32m', text: `TEST PASSED🥳🥳🥳\n`},
-    {type: 'failure', color: '\x1b[33m', text: `TEST FAILED💥💥💥\n`},
-    {type: 'actual-label', color: '\x1b[36m', text: `result:\n`},
-    {type: 'actual-value', color: '', text: `${inspect(actual)}\n`},
-    {type: 'expected-label', color: '\x1b[36m', text: `expected:\n`},
-    {type: 'expected-value', color: '', text: `${inspect(expected)}\n`},
-    {type: 'after', color: '', text: `\n----------`},
-  ];
-  
-  const resetColor = '\x1b[0m';
-  let message = '';
-  for (const object in config) {
-    if (config[object].type === 'success' && !hasEquality) continue; // Skip this message
-    if (config[object].type === 'failure' && hasEquality) continue; // Skip this message
-    message += config[object].color + config[object].text + resetColor;
-  }
-  return message;
-};
-
-/**
  * Function that compares 2 arrays for equality and returns the result
  * @param {Array<string|number|boolean>} array1 - The first array to compare
  * @param {Array<string|number|boolean>} array2 - The second array to compare
@@ -65,9 +19,4 @@ const eqArrays = (array1, array2) => {
   return true;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+module.exports = eqArrays;
