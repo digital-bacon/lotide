2c5e512df11f69c8f1a58d0bcc5a93dec659e70d
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 13:15:12 -0700

    Fix: function to handle edge cases undefined and empty argument

--- head.js
+++ head.js
@@ -4,6 +4,11 @@
  * @param {Array} array - An array containing one or more elements
  * @returns {*|undefined} The first element from the array, or undefined
  */
-const head = (array) => Array.isArray(array) && array.length > 0 ? array[0] : undefined;
+const head = (array) => {
+  if (!Array.isArray(array)) return undefined;
+  if (array.length <= 0) return undefined;
+  const result = array[0];
+  return result;
+}
 
 module.exports = head;
