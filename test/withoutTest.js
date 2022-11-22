c5eb8e2215d948fa13a1100bee7797baef8eb192
Mark Bieganek <mark.bieganek@telus.com>
2022-11-21 19:07:48 -0700

    Refactor to Mocha + Chai assertion tests in test/

---
+++ test/withoutTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const without = require('../without');
+
+describe("#without", () => {
+
+  it('should return [ 2, 3 ] when given [ 1, 2, 3 ]', () => {
+    const array1 = [ 1, 2, 3 ];
+    const array2 = [ 1 ];
+    const expectedResult = [ 2, 3 ];
+    assert.deepEqual(without(array1, array2), expectedResult);
+  });
+  
+  it('should not mutate the original array', () => {
+    const wordsBefore = ["hello", "world", "lighthouse"];
+    const array2 = ["lighthouse"];
+    const wordsAfter = ["hello", "world", "lighthouse"];
+    without(wordsAfter, array2)
+    assert.deepEqual(wordsBefore, wordsAfter);
+  });
+  
+});
--- without.js
+++ without.js
@@ -17,13 +17,4 @@ const without = (sourceItems, removeItems) => {
   return output;
 };
 
-// // TEST CASES
-// console.log(without([1, 2, 3], [1])); // => [2, 3]
-// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
 module.exports = without;
